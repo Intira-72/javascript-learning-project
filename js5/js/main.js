d38cfcbc4a35abedf85afa11ed78f4dc0afb7f88
Intira-72 <intira.vinit@gmail.com>
2023-07-24 10:00:55 +0700

    JS5 Math and Generate a Random Number

--- js5/js/main.js
+++ js5/js/main.js
@@ -2,3 +2,15 @@
 console.log(Math.PI);
 console.log(Math.trunc(Math.PI));
 console.log(Math.round(3.5));
+console.log(Math.floor(3.5));
+console.log(Math.pow(2, 4));
+console.log(Math.min(2, 4, 8));
+console.log(Math.max(2, 4, 8));
+
+// Math random from 1 to 10
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
