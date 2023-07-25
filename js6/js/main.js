c9ffa3ce01a3ee29042225bfe17a224b99d12cdd
Intira-72 <intira.vinit@gmail.com>
2023-07-25 10:22:42 +0700

    Problem Solving with Code - First Coding Challenge

---
+++ js6/js/main.js
@@ -0,0 +1,5 @@
+// Your First Code Challenge
+
+// Write code that will return a random letter from your name
+const m_name = "Intira"
+console.log(m_name.charAt(Math.floor(Math.random() * m_name.length)))
