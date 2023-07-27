a0e5481271f8704092ea9c48b06569d8da33432a
Intira-72 <intira.vinit@gmail.com>
2023-07-27 12:42:01 +0700

    JS User Input

---
+++ js10 - User Input/js/main.js
@@ -0,0 +1,18 @@
+// User Input
+
+// Example 1
+// alert("Hello, World!")
+
+// Example 2
+// let aws = confirm("OK === True\nCancel === False")
+// console.log(aws)
+
+// Example 3
+let myName = prompt("Please enter your name :");
+const msgError = "You didn't enter your name."
+
+if (myName) {
+    console.log(myName)
+} else {
+    console.log(msgError)
+}
